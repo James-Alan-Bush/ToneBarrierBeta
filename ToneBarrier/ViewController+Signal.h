008054b1474a93b744457de683e60a1ae4eb4cb4
James Alan Bush <jamesalanbush1972@gmail.com>
2023-02-23 12:59:45 -0500

    Creating random and distributed tone-duration and frequency generators

--- ToneBarrier/ViewController+Signal.h
+++ ToneBarrier/ViewController+Signal.h
@@ -16,8 +16,8 @@ NS_ASSUME_NONNULL_BEGIN
 static const float high_frequency = 1750.f;
 static const float low_frequency  = 500.f;
 
-static __inline__ CGFloat random_float_between(CGFloat a, CGFloat b) {
-    return a + (b - a) * ((CGFloat) random() / (CGFloat) RAND_MAX);
+static __inline__ simd_double1 random_float_between(simd_double1 a, simd_double1 b) {
+    return a + (b - a) * ((simd_double1) random() / (simd_double1) RAND_MAX);
 }
 
 static simd_double1 (^scale)(simd_double1, simd_double1, simd_double1, simd_double1, simd_double1) = ^ simd_double1 (simd_double1 val_old, simd_double1 min_new, simd_double1 max_new, simd_double1 min_old, simd_double1 max_old) {
@@ -50,40 +50,131 @@ static AVAudioFormat * (^audio_format)(void) = ^ AVAudioFormat * {
 // envelope = amplitude * tremolo
 // signal = envelope * frequency
 
-static AVAudioSourceNodeRenderBlock (^audio_renderer)(AVAudioFrameCount) = ^ AVAudioSourceNodeRenderBlock (AVAudioFrameCount frame_count) {
+static __inline__ typeof(simd_double1(^)(simd_double1)) rescaler (simd_double1 old_min, simd_double1 old_max, simd_double1 new_min, simd_double1 new_max) {
+    return ^ simd_double1 (simd_double1 value) {
+        return (simd_double1)(value = (new_max - new_min) * (value - old_min) / (old_max - old_min) + new_min);
+    };
+}
+
+static __inline__ typeof(simd_double1(^)(simd_double1)) distributor (simd_double1 mean, simd_double1 standard_deviation) {
+    simd_double1 variance = ((2.f * 0.5f) / M_PI) * (asin(sin(((2.f * M_PI) / 2.f) * mean)));
+    simd_double1 offset = (variance * 0.5f) * standard_deviation;
+    typeof(simd_double1(^)(simd_double1)) rescale = rescaler((mean - offset), (mean + offset), 0.f, 1.f);
+    return ^ (simd_double1 * offset_t, typeof(simd_double1(^(* restrict))(simd_double1)) rescale_t) {
+        return ^ simd_double1 (simd_double1 time) {
+            return (simd_double1)(time = ((*rescale_t)(time)));
+        };
+    }(&offset, &rescale);
+}
+
+static simd_double1 (^(^randomize)(void))(void) = ^{
+    srand48((unsigned long)clock());
+    static simd_double1 random;
+    return ^ (simd_double1 * random_t) {
+        return ^ simd_double1 {
+            return (simd_double1)(*random_t = (drand48()));
+        };
+    }(&random);
+};
+
+typedef typeof(simd_double1(^)(void)) random_generator;
+typedef typeof(simd_double1(^(* restrict))(void)) random_n_t;
+static simd_double1 (^(^(^(^generate_random)(simd_double1(^)(void)))(simd_double1(^)(simd_double1)))(simd_double1(^)(simd_double1)))(void) = ^ (simd_double1(^randomize)(void)) {
+    return ^ (simd_double1(^distribute)(simd_double1)) {
+        return ^ (simd_double1(^rescale)(simd_double1)) {
+            return ^ simd_double1 {
+                return rescale(distribute(randomize()));
+            };
+        };
+    };
+};
+
+static AVAudioSourceNodeRenderBlock (^audio_renderer)(unsigned long) = ^ AVAudioSourceNodeRenderBlock (unsigned long frame_count) {
     GKMersenneTwisterRandomSource * randomizer = [[GKMersenneTwisterRandomSource alloc] initWithSeed:(unsigned long)clock()];
     GKGaussianDistribution * distributor = [[GKGaussianDistribution alloc] initWithRandomSource:randomizer mean:(high_frequency / .75) deviation:low_frequency];
-    __block simd_double2x2 frequencies = simd_matrix_from_rows(simd_make_double2([distributor nextInt], [distributor nextInt]), simd_make_double2([distributor nextInt], [distributor nextInt]));
+
+    simd_double1 (^frequency_distributor)(simd_double1) = distribute(0.75f, 0.125f);
+    random_generator random_frequency_generator = generate_random(randomize())(frequency_distributor)(rescale);
+    static simd_double1 frequency_random[3];
+    static simd_double2x2 frequencies, theta_increments;
+    simd_double1 phase_angular_unit = (simd_double1)(M_PI_DBL / frame_count);
+    static simd_double2x2 signal_samples, thetas;
     
-    static AVAudioFramePosition   frame_position;
-    static AVAudioFramePosition * frame_position_t = &frame_position;
-    static simd_double1   time;
-    static simd_double1 * time_t = &time;
-    static simd_uint1 toggle;
+    static unsigned long   frame_position;
+    static unsigned long * frame_position_t = &frame_position;
+    // To-Do: generate a random value for the split frame at element 0
+    //        use gaussian_distribution to generate the split frame at element[1]:
+    //            value = element[0];
+    //            standard deviation = random;
+    //            mean = 0.5?
+    static simd_double1 split_frame, distributed_split_frame;
+    static unsigned long split_frames[2];
+    
+    //    static simd_double1   time;
+    //    static simd_double1 * time_t = &time;
+//
+    
     
     return ^ OSStatus (BOOL *isSilence, const AudioTimeStamp *timestamp, AVAudioFrameCount frameCount, AudioBufferList *outputData) {
-        static simd_double2x2 signal_samples;
-        AVAudioFramePosition split_frame = frame_count * 0.25;
-        
         for (AVAudioFramePosition frame = 0; frame < frameCount; frame++) {
-            (*frame_position_t ^ frame_count) && ^ AVAudioFramePosition {
-                signal_samples = matrix_scale((*time_t = 0.f + ((*frame_position_t - 0.f) * (1.f - 0.f)) / (~-frame_count - 0.f)), frequencies);
-                signal_samples = simd_matrix_from_rows(_simd_sinpi_d2(signal_samples.columns[0]),
-                                                       _simd_sinpi_d2(signal_samples.columns[1]));
-                !(*frame_position_t ^ split_frame) && (toggle ^= 1);
-                *((Float32 *)((Float32 *)((outputData->mBuffers + 0))->mData) + frame) = signal_samples.columns[0][toggle];
-                *((Float32 *)((Float32 *)((outputData->mBuffers + 1))->mData) + frame) = signal_samples.columns[1][toggle];
+            ({
+                !(*frame_position_t ^ frame_count) && ^ AVAudioFramePosition {
+                frequency_random[0] = gaussian_distribution(0.5, 0., random_float_between(0.1f, )
+                    frequency_random[1] = (simd_double1)random_float_between(low_frequency, high_frequency);
+                    frequency_random[2] = (simd_double1)random_float_between(low_frequency, high_frequency);
+                    frequency_random[3] = (simd_double1)random_float_between(low_frequency, high_frequency);
+                    frequencies = simd_matrix_from_rows(simd_make_double2(frequency_random[0],
+                                                                          gaussian_distribution((simd_double1)random_float_between(low_frequency, high_frequency), (simd_double1)random_float_between(low_frequency, high_frequency), frequency_random[0])),
+                                                        simd_make_double2(gaussian_distribution((simd_double1)random_float_between(low_frequency, high_frequency), (simd_double1)random_float_between(low_frequency, high_frequency), root_frequency))));
+                    theta_increments = matrix_scale(phase_angular_unit, frequencies);
+                    thetas = theta_increments;
+                    split_frame = (simd_double1)random_float_between(0.125f, 0.875f);
+                    distributed_split_frame = gaussian_distribution((simd_double1)random_float_between(0.125f, 0.875f), (simd_double1)random_float_between(0.125f, 0.875f), split_frame);
+                    printf("\nsplit_frame == %f\ndistributed_split_frame == %f\n", split_frame, distributed_split_frame);
+                    split_frames[0] = frame_count * 0.25;//(frame_count * split_frame);
+                    split_frames[1] = frame_count * 0.75;//(frame_count * distributed_split_frame);
+                    return (*frame_position_t = 0);
+                }();
                 
-                return (*frame_position_t)++;
-            }();
+                (*frame_position_t ^ frame_count) &&
+                ^ AVAudioFramePosition {
+                    signal_samples = simd_matrix_from_rows(_simd_sin_d2(simd_make_double2((simd_double2)thetas.columns[0])),
+                                                           _simd_sin_d2(simd_make_double2((simd_double2)thetas.columns[1])));
+                    
+                    // Transition combinations:
+                    // - transition from one tone to another on the same channel
+                    //      - mix the two tones
+                    //      - balance over time weighted by duration coefficient [balancer would be an equal trade-off, which may be limiting)
+                    // - transition from one tone to another from one channel to another
+                    //      - mix the two tones
+                    //      - add mix to both channels
+                    //      - balance over time in opposite directions in each channel
+                    
+                    // Ease-in/out combinations:
+                    // - the envelope between tone pairs
+                    //      - applied at the end
+                    // - the envelope between tones in a pair
+                    //      - applied to the duration coefficient only
+                    
+                    
+                    //                  signal_samples = matrix_scale(1.0 - (*time_t = 0.f + ((*frame_position_t - 0.f) * (1.f - 0.f)) / (~-frame_count - 0.f)), frequencies);
+                    //                  simd_double1   tone_pair_amplitude_envelope = (simd_double1)sinf(*time_t * M_PI);
+                    //                  signal_samples = matrix_scale(tone_pair_amplitude_envelope, (simd_matrix_from_rows(_simd_sinpi_d2(signal_samples.columns[0]),
+                    //                                                                                                     _simd_sinpi_d2(signal_samples.columns[1]))));
+                    
+                    
+//                    (*frame_position - split_frame) && (toggle ^= 1);
+                    *((Float32 *)((Float32 *)((outputData->mBuffers + 0))->mData) + frame) = simd_mix(signal_samples.columns[0][0], signal_samples.columns[0][1], (*frame_position_t < split_frames[0]));
+                    *((Float32 *)((Float32 *)((outputData->mBuffers + 1))->mData) + frame) = simd_mix(signal_samples.columns[1][0], signal_samples.columns[1][1], (*frame_position_t < split_frames[1]));
 
-            !(*frame_position_t ^ frame_count) && ^ AVAudioFramePosition {
-                frequencies = simd_matrix_from_rows(simd_make_double2([distributor nextInt], [distributor nextInt]), simd_make_double2([distributor nextInt], [distributor nextInt]));
-                return (*frame_position_t = 0);
-            }();
+//                    *((Float32 *)((Float32 *)((outputData->mBuffers + 1))->mData) + frame) = signal_samples.columns[1][(*frame_position_t < split_frame)];
+                    
+                    thetas  = simd_add(thetas, theta_increments);
+                    
+                    return (*frame_position_t)++;
+                }();
+            });
         }
-        //        printf("*frame_position_t  == %lld\n", *frame_position_t);
-        //        printf("*time_t == %f\n", *time_t);
         
         return (OSStatus)noErr;
     };
